8b312be25e5b9fd93a63600ff2807740864db9d3
bodziowagh <k.wolinski@univapay.com>
2018-09-05 18:25:18 +0900

    Asteroids and colistion detection prototype

--- src/index.js
+++ src/index.js
@@ -24,27 +24,65 @@ const KEY_CODE = {
 };
 
 const colors = {
-    cockpit: 0xdddddd,
-    body: 0xdddddd,
+    board:      0x853004,
+    cockpit:    0xdddddd,
+    body:       0xdddddd,
+    missile:    0xcccccc,
+    asteroid:   0x777777
 };
 
 const BOARD = {
-    minPositionX: -17,
-    maxPositionX:  17
+    minPositionX: -40,
+    maxPositionX:  40
 };
 
 let renderer, camera, scene, controls;
 let hemisphereLight, shadowLight, ambientLight, pointLight;
-let spaceship, board;
+let spaceship, board, asteroids;
 
 let time = 0;
+
+function Asteroid(position) {
+    this.radius = 10;
+    this.mesh = new THREE.Object3D();
+
+    let geometry = new THREE.SphereGeometry(this.radius);
+    let material = new THREE.MeshPhongMaterial({
+        color: colors.asteroid,
+        flatShading: THREE.SmoothShading
+    });
+
+    // TODO: improve randomizer mechanism
+    for (let i = 0; i < geometry.vertices.length; i ++) {
+        const vertex = geometry.vertices[i];
+        const max = 1;
+
+        vertex.x += Math.random() * max - max / 2;
+        vertex.y += Math.random() * max - max / 2;
+        vertex.z += Math.random() * max - max / 2;
+    }
+
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.mesh.position.set(position.x, position.y, position.z);
+
+    // this.mesh.updateMatrix();
+    // this.mesh.geometry.applyMatrix(this.mesh.matrix);
+    // this.mesh.matrix.identity();
+    //
+    // this.mesh.position.set(0, 0, 0);
+    // this.mesh.rotation.set(0, 0, 0);
+    // this.mesh.scale.set(1, 1, 1);
+
+    this.mesh.name = "asteroid-" + Date.now();
+}
 
 function Missile(spaceshipPosition) {
     this.mesh = new THREE.Object3D();
 
     let geometry = new THREE.Geometry();
     let material = new THREE.MeshPhongMaterial({
-        color: colors.body,
+        color: colors.missile,
         flatShading: THREE.SmoothShading
     });
 
@@ -78,25 +116,28 @@ function Missile(spaceshipPosition) {
     this.mesh = new THREE.Mesh(geometry, material);
 
     this.mesh.scale.set(0.2, 0.2, 0.2);
-    this.mesh.rotation.x = -Math.PI / 2;
-    this.mesh.position.x = spaceshipPosition.x;
-    this.mesh.position.z = spaceshipPosition.z - 1;
+    this.mesh.rotation.x = -Math.PI / 2 + 0.2;
 
-    this.mesh.updateMatrix();
-    this.mesh.geometry.applyMatrix(this.mesh.matrix);
-    this.mesh.matrix.identity();
+    this.mesh.position.set(
+        spaceshipPosition.x,
+        spaceshipPosition.y - 15,   // TODO hack!
+        spaceshipPosition.z - 1
+    );
 
-    this.mesh.position.set(0, 0, 0);
-    this.mesh.rotation.set(0, 0, 0);
-    this.mesh.scale.set(1, 1, 1);
+    // this.mesh.updateMatrix();
+    // this.mesh.geometry.applyMatrix(this.mesh.matrix);
+    // this.mesh.matrix.identity();
+    //
+    // this.mesh.position.set(0, 0, 0);
+    // this.mesh.rotation.set(0, 0, 0);
+    // this.mesh.scale.set(1, 1, 1);
 
     // Generate an unique id
     this.mesh.name = "missile-" + Date.now();
 
     // Init missile's properties
     this.range = 200;
-    this.speed = 1.2;
-
+    this.speed = 0.01;
 }
 
 function init() {
@@ -109,7 +150,7 @@ function init() {
 
 		scene = new THREE.Scene();
 
-		camera.position.set(0, 10, 15);
+		camera.position.set(0, 10, 50);
 		camera.lookAt(new THREE.Vector3(0, 5, 0));
 
 		// TODO: remove for release
@@ -219,7 +260,7 @@ function init() {
 
         var wings = new THREE.Mesh(geomWings, matWings);
 
-        // TODO: unneccesary
+        // TODO: unnecessary
         this.mesh.add(wings);
 
         const mergedGeometry = new THREE.Geometry();
@@ -236,6 +277,8 @@ function init() {
 
         this.mesh.scale.set(0.1, 0.1, 0.1);
         this.mesh.rotation.y = Math.PI / 2;
+        this.mesh.rotation.x = 0.3;
+        this.mesh.position.y -= 15;
 
         this.mesh.updateMatrix();
         this.mesh.geometry.applyMatrix(this.mesh.matrix);
@@ -269,7 +312,7 @@ function init() {
 
         const boardGeometry = new THREE.CylinderGeometry(400, 400, 500, 20, 20);
         const boardMaterial = new THREE.MeshPhongMaterial({
-            color: 0x853004
+            color: colors.board
         })
 
         for (var i = 0; i < boardGeometry.vertices.length; i++) {
@@ -296,10 +339,17 @@ function init() {
 	    scene.add(board.mesh);
     }
 
+    function initAsteroids() {
+	    asteroids = [];
+	    asteroids.maxCount = 20;
+	    asteroids.cooldown = 0;
+    }
+
 	initWebGL();
     initLights();
 	initSpaceship();
     initBoard();
+    initAsteroids();
 }
 
 function render() {
@@ -307,7 +357,7 @@ function render() {
 }
 
 function updateBoard() {
-    board.mesh.rotation.x += 0.01;
+    board.mesh.rotation.x += 0.01;  // TODO: config rotation speed etc
 }
 
 function updateSpaceship() {
@@ -349,23 +399,85 @@ function updateSpaceship() {
     spaceship.mesh.rotation.z = -(speed / spaceship.maxSpeed) * Math.PI / 10;
 }
 
+// TODO: missiles should be orbiting around the board
 function updateMissiles () {
     for (let i = 0; i < spaceship.missles.length; i++) {
         const missile = spaceship.missles[i];
 
-        missile.mesh.position.z -= missile.speed;
+        missile.mesh.position.sub(board.mesh.position);
+        missile.mesh.position.applyAxisAngle(new THREE.Vector3(-1, 0, 0), missile.speed);
+        missile.mesh.position.add(board.mesh.position);
 
         // TODO: make it more generic (3d instead of 1d)
-        if (missile.mesh.position.z < - missile.range) {
-            spaceship.missles.splice(i, 1);
-            scene.remove(scene.getObjectByName(missile.mesh.name));
-        }
+        // if (missile.mesh.position.z < - missile.range) {
+        //     spaceship.missles.splice(i, 1);
+        //     scene.remove(scene.getObjectByName(missile.mesh.name));
+        // }
     }
 
     if (spaceship.missles.cooldown > 0) {
         spaceship.missles.cooldown -= 1;
     } else {
         spaceship.missles.cooldown = 0;
+    }
+}
+
+function updateAsteroids() {
+    if (!asteroids.cooldown && asteroids.length < asteroids.maxCount) {
+        const initialPosition = {
+            x: board.mesh.position.x + (BOARD.maxPositionX - BOARD.minPositionX) *Math.random() + BOARD.minPositionX,
+            y: board.mesh.position.y - 410,
+            z: board.mesh.position.z
+        }
+        const asteroid = new Asteroid(initialPosition);
+
+        asteroids.push(asteroid);
+        scene.add(asteroid.mesh);
+
+        asteroids.cooldown = 50;
+    }
+    // TODO: generate new asteroids if required
+    // TODO: rotate/move the asteroids
+    // TODO: remove old asteroids
+    // TODO: detect collision
+
+    asteroids.forEach(function(asteroid, ai) {
+        // Animate asteroids
+        const rotationIncrement = 0.02;
+
+        asteroid.mesh.rotation.x += rotationIncrement;
+        asteroid.mesh.rotation.z += rotationIncrement / 2;
+
+        // Rotate around the center
+        asteroid.mesh.position.sub(board.mesh.position);
+        asteroid.mesh.position.applyAxisAngle(new THREE.Vector3(1, 0, 0), 0.01);
+        asteroid.mesh.position.add(board.mesh.position);
+
+        // Collision with missiles
+        spaceship.missles.forEach(function(missile, mi) {
+            const m = missile.mesh.position;
+            const a = asteroid.mesh.position;
+            const r = asteroid.radius;
+
+            if (
+                (m.x > a.x - r && m.x < a.x + r) &&
+                (m.y > a.y - r && m.y < a.y + r) &&
+                (m.z > a.z - r && m.z < a.z + r)
+            ) {
+                // TODO: explosion
+                asteroids.splice(ai, 1)
+                scene.remove(scene.getObjectByName(asteroid.mesh.name));
+
+                spaceship.missles.splice(mi, 1);
+                scene.remove(scene.getObjectByName(missile.mesh.name));
+            }
+        })
+    });
+
+    if (asteroids.cooldown > 0) {
+        asteroids.cooldown -= 1;
+    } else {
+        asteroids.cooldown = 0;
     }
 }
 
@@ -379,6 +491,7 @@ function animate() {
     updateBoard();
     updateSpaceship();
     updateMissiles();
+    updateAsteroids();
 
     updateInterface();
 
